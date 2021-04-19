e3a81edf61e368610bb61b834fbc1c863e472bce
Erubin <magomedamin90@ya.ru>
2021-04-19 14:11:54 +0300

    Home task 1.2 Добавил обратботку максимального значечения

--- Home task 1/src/Kth.java
+++ Home task 1/src/Kth.java
@@ -1,27 +1,30 @@
 import java.util.Scanner;
-// ДЗ 1
+
 public class Kth {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         long k = in.nextLong();
         long numStep = 9;
         byte symbolsNum = 1;
-        long indexMinNum = numStep * symbolsNum;
         long minNum = 0;
         long num;
         byte resChar = 0;
-        while (indexMinNum <= k) {
+        while (k >= 0) {
+            k -= symbolsNum * numStep;
             symbolsNum++;
             numStep *= 10;
-            indexMinNum += numStep * symbolsNum;
         }
-        indexMinNum -= symbolsNum * numStep;
+        symbolsNum--;
+        numStep /= 10;
+        if (k < 0) {
+            k += symbolsNum * numStep;
+        }
         for (int i = 1, s = 1; i < symbolsNum; i++) {
             minNum += 9L * s;
             s *= 10;
         }
-        num = minNum + 1 + (k - indexMinNum) / symbolsNum;
-        for (int i = 0; i < symbolsNum - (k - indexMinNum) % symbolsNum; i++) {
+        num = minNum + 1 + k / symbolsNum;
+        for (int i = 0; i < symbolsNum - k % symbolsNum; i++) {
             resChar = (byte) (num % 10);
             num /= 10;
         }
