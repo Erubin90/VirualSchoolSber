348ca9783730be98a9bcaeccc0c1162f4781d795
Erubin <magomedamin90@ya.ru>
2021-05-09 16:19:36 +0300

    HomeWork 7

---
+++ src/com/homeWork7/Main.java
@@ -0,0 +1,12 @@
+package com.homeWork7;
+
+public class Main {
+    public static void main(String[] args) {
+        MyIntegerSecond intSecond1 = new MyIntegerSecond(123);
+        MyIntegerSecond intSecond2 = new MyIntegerSecond(231);
+        MyIntegerSecond intSecond3 = new MyIntegerSecond(31);
+        MyIntegerSecond intSecond4 = new MyIntegerSecond(31);
+        MyIntegerSecond intSecond5 = new MyIntegerSecond(31);
+
+    }
+}
