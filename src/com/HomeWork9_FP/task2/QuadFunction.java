53e6f1e2e722f1b264d421af9e3b342321083239
Erubin <magomedamin90@ya.ru>
2021-05-26 13:18:21 +0300

    Home Work 9 Функциональное програмирование

---
+++ src/com/HomeWork9_FP/task2/QuadFunction.java
@@ -0,0 +1,5 @@
+package com.HomeWork9_FP.task2;
+
+public interface QuadFunction<T, U, C, D, R> {
+    R apply(T t, U u, C c, D d);
+}
---
+++ src/com/HomeWork9_FP/task2/TriFunction.java
@@ -0,0 +1,5 @@
+package com.HomeWork9_FP.task2;
+
+public interface TriFunction<T, U, C, R> {
+    R apply (T t, U u, C c);
+}
